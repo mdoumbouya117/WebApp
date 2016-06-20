edcf28802cce779b52ab143cb131a21160943e16
Mohamed Doumbouya <mdoumbouya117@gmail.com>
2016-06-20 07:15:48 +0200

    folders config & index init

--- test/index.php
+++
@@ -1,4 +0,0 @@
-<?php include('inc/user/init.inc');?>
-<?php include('inc/template/page_top.inc');?>
-<?php include('inc/template/content.inc'); ?>
-<?php include('inc/template/page_bottom.inc'); ?>
